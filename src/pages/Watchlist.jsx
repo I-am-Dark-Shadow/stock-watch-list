cf39200ebc78b793c1a34cdd4d0b4a384f655e74
Suvrodip Chakroborty <suvrodipchakroborty01@gmail.com>
2025-09-10 06:44:14 +0530

    first commit

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -113,6 +113,7 @@ const Watchlist = () => {
             <img src="./logoname.webp" alt="Logo Name"
               width={200}
               height={60}
+              loading="lazy"
               className="w-[200px] lg:w-[250px] ml-[-5px]" />
             <p className="text-xs text-slate-400">Track, filter, and explore live market movers</p>
           </div>
